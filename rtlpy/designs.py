106332bfdfc89c73be2c7fa45afd2ccf713c10cc
allRisc <benjamin.j.davis96@gmail.com>
2022-10-18 17:32:53 -0700

    First commit

---
+++ rtlpy/designs.py
@@ -0,0 +1,52 @@
+##########################################################################
+# A module in the library to hold all of the classes necessary for design#
+#   creation and some functions to manipulate them                       #
+# Copyright (C) 2022, Benjamin Davis                                     #
+#                                                                        #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. #
+##########################################################################
+
+class Field :
+  def __init__(self) :
+    raise NotImplementedError("Field class Not Implemented")
+
+
+class Register :
+  def __init__(self) :
+    raise NotImplementedError("Register class Not Implemented")
+
+
+class AddressBlock :
+  def __init__(self) :
+    raise NotImplementedError("AddressBlock class Not Implemented")
+
+
+class MemoryMap :
+  def __init__(self) :
+    raise NotImplementedError("MemoryMap class Not Implemented")
+
+
+class Parameter :
+  def __init__(self) :
+    raise NotImplementedError("Parameter class Not Implemented")
+
+
+class Port :
+  def __init__(self) :
+    raise NotImplementedError("Port class Not Implemented")
+
+
+class Module :
+  def __init__(self) :
+    raise NotImplementedError("Module class Not Implemented")
